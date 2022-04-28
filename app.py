258993d46d52fc9fad4bf3b5af7287bf5f6be988
R.OJI <e1128@e-mall.co.jp>
2022-04-28 13:40:26 +0900

    test

---
+++ app.py
@@ -0,0 +1,23 @@
+# インストールした「flask.py」から「Flask」Classをimport
+from flask import Flask
+
+# Flaskクラスをnewしてappに代入
+app = Flask(__name__)
+
+
+# urlとして”/hello”がgetメソッドで呼ばれると定義した関数が実行される
+@ app.route('/hello')
+def hello():
+    hello = "Hello world"
+    return hello
+
+
+@ app.route('/')
+def main():
+    hello = "Hello world"
+    return hello
+
+
+if __name__ == "__main__":
+    # runメソッドでビルトインサーバーが走る
+    app.run()
